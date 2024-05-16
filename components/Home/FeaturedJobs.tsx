591c58bc73cf063d659154355ab0b33afcfef41a
hadiRazal <hadhirasal22@gmail.com>
2024-05-16 15:52:18 +0530

    updates in product card

--- components/Home/FeaturedJobs.tsx
+++ components/Home/FeaturedJobs.tsx
@@ -1,28 +1,62 @@
-import React from 'react'
+"use client"
+
+import React, { useRef } from 'react'
 import { CiCircleChevRight } from 'react-icons/ci'
 import JobCard from '../JobCard'
+import { GoChevronLeft, GoChevronRight } from 'react-icons/go'
 
 const FeaturedJobs = () => {
+
+    const scrollContainerRef = useRef<HTMLDivElement>(null);
+
+
+    const handleScrollRight = () => {
+        try {
+            const scrollAmount = 300;
+            if (scrollContainerRef.current) {
+                scrollContainerRef.current.scrollBy({
+                    left: +scrollAmount,
+                    behavior: 'smooth',
+                });
+            }
+        } catch (error) {
+            console.error('Error scrolling:', error);
+        }
+    };
+
+    const handleScrollLeft = () => {
+        try {
+            const scrollAmount = 300;
+            if (scrollContainerRef.current) {
+                scrollContainerRef.current.scrollBy({
+                    left: -scrollAmount,
+                    behavior: 'smooth',
+                });
+            }
+        } catch (error) {
+            console.error('Error scrolling:', error);
+        }
+    };
+
     return (
-        <div className='flex items-center justify-center gap-7 flex-col w-full px-3 md:px-10 md:py-12 py-7 '>
-            <div className='flex items-center sm:justify-between justify-center  w-full'>
+        <div className=' flex items-center justify-center gap-7 flex-col w-full px-5 md:px-10 md:py-12 py-7 '>
+            <div className='flex items-center justify-center  w-full'>
                 <h1 className="lg:text-[40px] md:text-[35px] sm:text-[30px] xs:text-[25px]  font-semibold">Featured <span className="text-rheinland-yellow">Jobs</span> </h1>
-                <span className="text-rheinland-yellow sm:flex items-center justify-center flex-wrap gap-3 cursor-pointer hidden ">
-                    Show all jobs
-                    <CiCircleChevRight />
-                </span>
             </div>
-            <div className="flex items-center justify-center gap-2 flex-wrap">
+            <div className='relative w-full'>
+                <GoChevronLeft onClick={handleScrollLeft} className='md:hover:scale-110 z-10 absolute top-[170px] bg-black text-white  left-2 rounded-full cursor-pointer' size={30} />
+                <GoChevronRight onClick={handleScrollRight} className='md:hover:scale-110 z-10 absolute top-[170px] bg-black text-white right-2 rounded-full cursor-pointer' size={30} />
+            </div>
+
+            <div ref={scrollContainerRef} className={`relative flex py-5 overflow-scroll justify-start items-center gap-2 w-full hideScrollBar `}>
+                <JobCard />
+                <JobCard />
                 <JobCard />
                 <JobCard />
                 <JobCard />
                 <JobCard />
                 <JobCard />
             </div>
-            <span className="text-rheinland-yellow flex items-center justify-center flex-wrap gap-2 cursor-pointer sm:hidden text-[18px] ">
-                Show all jobs
-                <CiCircleChevRight />
-            </span>
         </div>
     )
 }
--- components/JobCard.tsx
+++ components/JobCard.tsx
@@ -1,20 +1,33 @@
 import Image from 'next/image'
 import React from 'react'
+import { CiLocationOn } from 'react-icons/ci'
+import { HiOutlineBuildingOffice2 } from 'react-icons/hi2'
 
 const JobCard = () => {
     return (
-        <div className='border  flex flex-row sm:flex-col items-start sm:justify-between sm:w-[30vw] w-full  sm:min-h-[20vh] min-h-[10vh] group rounded-sm px-3 py-6  cursor-pointer '>
+        <div className='border flex-col items-start justify-between w-[300px]  min-h-[200px] group rounded-sm px-3 py-6  cursor-pointer '>
             <div className='flex justify-between items-center w-full'>
                 <Image src={'/RheinlandLogoHeader.png'} alt='Logo' height={60} width={60} />
                 <span className='border px-2 py-1 rounded-sm'>Full Time</span>
             </div>
-            <div className='flex justify-start items-center font-light text-sm gap-2 w-full'>
-                <span className=''>Duoph</span>
-                <span></span>
-                <span className=''>Berlin,Germany</span>
+            <div className='flex justify-start items-center font-semibold gap-2 w-full'>
+                <span>Social Media Assistant</span>
             </div>
-            <div className='flex justify-start items-center font-light text-sm gap-2 w-full'>
-                <p className=''>Berlin,Germany</p>
+            <div className='flex justify-start items-center font-light text-sm gap-2 w-full py-1'>
+                <span className='flex items-center justify-center gap-[6px]'>
+                    <HiOutlineBuildingOffice2 />
+                    Duoph</span>
+                <span></span>
+                <span className='flex items-center justify-center gap-[6px]'>
+                    <CiLocationOn />
+                    Berlin,Germany
+                </span>
+            </div>
+            <div className='flex justify-start items-center  gap-2 w-ful py-2l'>
+                <p className=''>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Earum, assumenda!y...</p>
+            </div>
+            <div>
+
             </div>
         </div>
     )
