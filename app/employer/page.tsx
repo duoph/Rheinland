8174f15b01cbe4f17158ef89cfda4e6310484e65
Praveen Prasad <praveenprasad2510@gmail.com>
2024-05-27 19:12:07 +0530

    Updated Employer Dashboard

--- app/employer/page.tsx
+++ app/employer/page.tsx
@@ -1,31 +1,21 @@
-import Link from 'next/link'
-import React from 'react'
+import Link from "next/link";
+import React from "react";
 
 const EmployerPage = () => {
-    return (
-        <div className=' flex items-center justify-center h-screen'>
+  return (
+    <div className=" flex items-center justify-center h-screen">
+      <div className="flex flex-col items-center justify-center gap-3 text-white">
+        <Link
+          href={"/employer/job/create-job"}
+          className="bg-rheinland-red px-4 py-3 rounded-sm"
+        >
+          <button className="bg-rheinland-red px-4 py-3 rounded-sm">
+            Create Job
+          </button>
+        </Link>
+      </div>
+    </div>
+  );
+};
 
-            <div className='flex flex-col items-center justify-center gap-3 text-white' >
-
-                <Link href={'/employer/create-job'} className='bg-rheinland-red px-4 py-3 rounded-sm'>
-                    Create Job
-                </Link>
-                
-                <button className='bg-rheinland-red px-4 py-3 rounded-sm'>
-                    Create Job
-                </button>
-                <button className='bg-rheinland-red px-4 py-3 rounded-sm'>
-                    Create Job
-                </button>
-                <button className='bg-rheinland-red px-4 py-3 rounded-sm'>
-                    Create Job
-                </button>
-
-            </div>
-
-
-        </div>
-    )
-}
-
-export default EmployerPage
+export default EmployerPage;
