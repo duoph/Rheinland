be17eaab87f6d992b5c0a3932e091d7c349bbd41
Praveen Prasad <praveenprasad2510@gmail.com>
2024-06-09 02:26:31 +0530

    Updated Profile Component

--- app/employer/profile/page.jsx
+++ app/employer/profile/page.jsx
@@ -1,11 +1,27 @@
-import React from 'react'
+import React from "react";
 
 const page = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <div className="pt-20">
+      <h1>My Profile</h1>
+      {/* Logo  */}
 
-export default page
+      <div className="flex flex-row w-[100%]  cursor-pointer  gap-3 ">
+        <img src="/person-icon.jpg" alt="" className="w-16 rounded-[50px]" />
+        <button className="px-2 py-1  bg-rheinland-red text-white">
+          Browse
+        </button>
+      </div>
+      <form action="" className="flex flex-col ">
+        <label htmlFor="">Employer Name</label>
+        <input
+          type="text"
+          value={"Duoph Technologies"}
+          className="border-2 w-[380px] h-[50px] outline-none rounded-md px-2"
+        />
+      </form>
+    </div>
+  );
+};
+
+export default page;
