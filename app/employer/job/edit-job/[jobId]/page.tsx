dcd923e5564532c43838b2fab4eea24b2ba1f46a
Praveen Prasad <praveenprasad2510@gmail.com>
2024-06-09 03:06:30 +0530

    Created Sidebar and profile components for Admin Dashboard

---
+++ app/employer/job/edit-job/[jobId]/page.tsx
@@ -0,0 +1,118 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className='flex flex-col items-center justify-start pt-[90px] min-h-screen px-3 sm:px-5 gap-5 pb-20'>
+
+    <h1 className="lg:text-[40px] md:text-[35px] sm:text-[30px] xs:text-[25px] text-rheinland-red font-semibold">Edit Job</h1>
+
+    <form className=' sm:w-[500px] w-full flex flex-col items-center justify-center gap-5' >
+
+        <input
+            type='text'
+            name='title'
+            value=""
+            // onChange={}
+            placeholder='Title'
+            required
+            className="w-full border px-3 py-3 border-b rounded-sm focus:outline-none"
+        />
+
+        <input
+            type='text'
+            name='category'
+            value=""
+            // onChange={}
+            placeholder='Category'
+            required
+            className="w-full border px-3 py-3 border-b rounded-sm focus:outline-none"
+        />
+        <textarea
+            name='description'
+            // value={job.description}
+            // onChange={}
+            placeholder='Description'
+            required
+            className="w-full border px-3 py-3 border-b rounded-sm focus:outline-none"
+        />
+
+        <select
+            name='gender'
+            // value={job.gender}
+            // onChange={}
+            required
+            className="w-full border px-3 py-3 border-b rounded-sm focus:outline-none"
+        >
+            <option value='' disabled>Select Gender</option>
+            <option value='Male'>Male</option>
+            <option value='Female'>Female</option>
+            <option value='Any'>Any</option>
+
+        </select>
+
+        <input
+            type='text'
+            name='skills'
+            // value={job.skills}
+            // onChange={}
+            placeholder='Skills (comma-separated)'
+            className="w-full border px-3 py-3 border-b rounded-sm focus:outline-none"
+        />
+
+        <input
+            type='text'
+            name='minAge'
+            // value={job.minAge}
+            // onChange={}
+            placeholder='Minimum Age'
+            className="w-full border px-3 py-3 border-b rounded-sm focus:outline-none"
+        />
+
+        <input
+            type='text'
+            name='maxAge'
+            // value={job.maxAge}
+            // onChange={}
+            placeholder='Maximum Age'
+            className="w-full border px-3 py-3 border-b rounded-sm focus:outline-none"
+        />
+
+        <input
+            type='text'
+            name='state'
+            // value={job.state}
+            // onChange={}
+            placeholder='State'
+            className="w-full border px-3 py-3 border-b rounded-sm focus:outline-none"
+        />
+
+        <input
+            type='text'
+            name='location'
+            // value={job.location}
+            list="cities"
+            // onChange={}
+            placeholder='Location'
+            className="w-full border px-3 py-3 border-b rounded-sm focus:outline-none"
+        />
+
+        <datalist className='w-full' id="cities">
+            {/* {cities.map((city, index) => (
+                <option key={index} value={city} />
+            ))} */}
+        </datalist>
+
+
+        <button
+            type='submit'
+            className='px-5 py-3  w-full bg-rheinland-red text-white'
+            // disabled={isLoading}
+        >
+         Update   {/* {isLoading ? 'Creating...' : 'Create Job'} */}
+        </button>
+    </form>
+</div >
+  )
+}
+
+export default page
--- app/employer/page.tsx
+++ app/employer/page.tsx
@@ -1,31 +1,22 @@
-import Link from 'next/link'
-import React from 'react'
+import Link from "next/link";
+import React from "react";
+import { LiaUserTieSolid } from "react-icons/lia";
+import { HiOutlineBriefcase } from "react-icons/hi";
+import { GoPencil } from "react-icons/go";
+import { FaPowerOff } from "react-icons/fa6";
+import Sidebar from "./sidebar/page";
 
 const EmployerPage = () => {
-    return (
-        <div className=' flex items-center justify-center h-screen'>
-
-            <div className='flex flex-col items-center justify-center gap-3 text-white' >
-
-                <Link href={'/employer/create-job'} className='bg-rheinland-red px-4 py-3 rounded-sm'>
-                    Create Job
-                </Link>
-                
-                <button className='bg-rheinland-red px-4 py-3 rounded-sm'>
-                    Create Job
-                </button>
-                <button className='bg-rheinland-red px-4 py-3 rounded-sm'>
-                    Create Job
-                </button>
-                <button className='bg-rheinland-red px-4 py-3 rounded-sm'>
-                    Create Job
-                </button>
-
-            </div>
-
-
+  return (
+    <>
+      <div className="flex flex-row">
+        <Sidebar />
+        <div className="bg-[#f5f7fc] w-screen h-screen">
+          
         </div>
-    )
-}
+      </div>
+    </>
+  );
+};
 
-export default EmployerPage
+export default EmployerPage;
