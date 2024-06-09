fee9954d6ee5b9b2124fb9b5423683ac2c8e4f44
Praveen Prasad <praveenprasad2510@gmail.com>
2024-06-09 15:26:28 +0530

    Profile Component made responsive

--- app/employer/profile/page.jsx
+++ app/employer/profile/page.jsx
@@ -2,19 +2,18 @@ import React from "react";
 
 const page = () => {
   return (
-    <div className="pt-20">
+    <div className="pt-20 w-screen md: px-5 md:px-10 ">
       <h1 className="font-semibold text-xl">My Profile</h1>
       {/* Logo  */}
-
-      <div className="flex flex-row w-[100%] items-center cursor-pointer  gap-3 ">
+      <div className="flex flex-row w-[100%] items-center cursor-pointer  gap-3 py-5">
         <img src="/person-icon.jpg" alt="" className="w-16 rounded-[50px]" />
         <button className="px-2 py-1 h-[40px] bg-rheinland-red text-white">
           Browse
         </button>
       </div>
-      <form action="" className="flex flex-col ">
+      <form action="" className="flex flex-col gap-5 md:gap-8">
         {/* Email & Name  */}
-        <div className="flex flex-row gap-10">
+        <div className="flex flex-col gap-5 md:flex-row md:gap-10 ">
           {/* Employer Name */}
           <div className="flex flex-col justify-start">
             <label htmlFor="" className="font-semibold">
@@ -40,7 +39,7 @@ const page = () => {
           </div>
         </div>
         {/* Ph No & Website */}
-        <div className="flex flex-row gap-10">
+        <div className="flex flex-col gap-5 md:flex-row md:gap-10 ">
           {/* Phone Number*/}
           <div className="flex flex-col justify-start">
             <label htmlFor="" className="font-semibold">
@@ -66,8 +65,7 @@ const page = () => {
             />
           </div>
         </div>
-        {/* About */}
-        <div className="flex flex-row gap-10">
+     
           {/* About*/}
           <div className="flex flex-col justify-start">
             <label htmlFor="" className="font-semibold">
@@ -77,10 +75,10 @@ const page = () => {
               rows="10"
               cols="50"
               placeholder="Write about your company..."
-              className="border-2 w-[760px] outline-none rounded-md px-2 py-2"
+              className="border-2 w-[380px] md:w-[760px] outline-none rounded-md px-2 py-2"
             />
           </div>
-        </div>
+        
         {/* Location  */}
 
         <div className="flex flex-col justify-start">
@@ -93,7 +91,7 @@ const page = () => {
             className="border-2 w-[380px] h-[50px] outline-none rounded-md px-2"
           />
         </div>
-        <div className="">
+        <div className="py-5">
           <button className="px-2 w-[100px] py-1 h-[50px] bg-rheinland-red text-white">
             Save
           </button>
