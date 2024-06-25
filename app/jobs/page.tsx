1637612d385d6d7c6e6a7d1a122c664f5ec0fa4e
hadi-razal <hadhirasal22@gmail.com>
2024-06-25 14:24:57 +0530

    added loading to jobs page

--- app/jobs/page.tsx
+++ app/jobs/page.tsx
@@ -4,19 +4,16 @@ import JobCard from '@/components/JobCard';
 import { Job } from '@/types';
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
-import { CiLocationOn, CiSearch } from 'react-icons/ci';
 
 const Jobs = () => {
-    const [jobs, setJobs] = useState<Job[] | []>(); // Correct type for jobs
+    const [jobs, setJobs] = useState<Job[]>([]);
 
     const fetchJobs = async () => {
         try {
-            const res = await axios.get('/api/job');
-            if (res.data.success === true) {
-                setJobs(res.data.jobs);
-            }
+            const { data } = await axios.get('/api/job');
+            if (data.success) setJobs(data.jobs);
         } catch (error) {
-            console.log(error);
+            console.error(error);
         }
     };
 
@@ -25,32 +22,44 @@ const Jobs = () => {
     }, []);
 
     return (
-        <div className='flex flex-col items-center justify-start pt-[80px] px-3'>
-            {/* Search bar */}
-            <div className="w-full md:w-full flex items-center justify-end lg:flex-row flex-col gap-3 bg-slate py-5 rounded-sm">
-                <div className="flex items-center justify-center bg-white px-2 w-full rounded-sm">
-                    <CiSearch size={24} />
-                    <input type="text" className="w-full px-3 py-3 border-b rounded-sm focus:outline-none" placeholder="Job Title or Keyword" />
+        <div className='flex flex-col items-center justify-start pt-[80px] px-3 h-screen'>
+            <div className='w-full md:w-full flex items-center justify-end lg:flex-row flex-col gap-3 bg-slate py-5 rounded-sm'>
+                <div className='flex items-center justify-center bg-white px-2 w-full rounded-sm'>
+                    <input
+                        type='text'
+                        className='w-full px-3 py-3 border-b rounded-sm focus:outline-none'
+                        placeholder='Job title or keyword'
+                    />
                 </div>
-                <div className="flex items-center justify-center bg-white px-2 w-full rounded-sm">
-                    <CiLocationOn size={24} />
-                    <input type="text" className="w-full px-3 py-3 border-b rounded-sm focus:outline-none" placeholder="Eg: Berlin" />
+                <div className='flex items-center justify-center bg-white px-2 w-full rounded-sm'>
+                    <input
+                        type='text'
+                        className='w-full px-3 py-3 border-b rounded-sm focus:outline-none'
+                        placeholder='E.g. Berlin'
+                    />
                 </div>
-                <button className="w-full md:w-2/6 bg-rheinland-red text-white rounded-sm px-3 py-3 flex items-center justify-center">
+                <button className='w-full md:w-2/6 bg-rheinland-red text-white rounded-sm px-3 py-3 flex items-center justify-center'>
                     Search
                 </button>
             </div>
 
-            {/* Displaying jobs with JobCard */}
-            <div className='flex items-center justify-center flex-wrap gap-3'>
-                {jobs?.map((job) => (
-                    <JobCard key={job?._id} job={job} /> // Assuming JobCard expects a job prop
-                ))}
-            </div>
+            {jobs.length === 0 ? (
+                <div className='flex items-center justify-center flex-wrap gap-3 h-2/3'>
+                    <p className='flex items-center justify-center'>Loading...</p>
+                </div>
+            ) : (
+                <div className='flex items-center justify-center flex-wrap gap-3'>
+                    {jobs.map((job) => (
+                        <JobCard key={job._id} job={job} />
+                    ))}
+                </div>
+            )}
 
-            <div className='flex items-center justify-center w-full py-5'>
-                <button className="w-[200px] bg-rheinland-red text-white rounded-sm px-3 py-3">Load More</button>
-            </div>
+            {jobs.length > 0 && (
+                <button className='w-[200px] bg-rheinland-red text-white rounded-sm px-3 py-3'>
+                    Load more
+                </button>
+            )}
         </div>
     );
 };
