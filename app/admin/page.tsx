cbf09443e796cadddd6bd4df62254ebc1d8bc174
Praveen Prasad <praveenprasad2510@gmail.com>
2024-06-26 14:54:33 +0530

    created Admin dashboard

---
+++ app/admin/page.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function adminPage() {
+    return (
+        // Sidebar 
+
+        <div className='pt-[110px] w-[300px] h-screen bg-red-300 flex flex-col px-5'>
+            <h1>Hello</h1>  
+
+            <p>Candidates</p>
+            <p>Applications</p>
+            <p>Companies</p>
+            <p>Jobs</p>
+            <p>Logout</p>
+        </div>
+    )
+}
+
+export default adminPage
