8defb657374b8047e07c84c01d549be7eb2d59f4
Praveen Prasad <praveenprasad2510@gmail.com>
2024-06-26 15:40:10 +0530

    Completed sidebarf

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,24 +1,50 @@
-import React from 'react'
+import React from "react";
 import { FaUserGraduate } from "react-icons/fa";
 import { IoDocumentTextSharp } from "react-icons/io5";
 import { HiMiniBuildingOffice2 } from "react-icons/hi2";
 import { MdWork } from "react-icons/md";
 import { FaPowerOff } from "react-icons/fa";
 
+
 function adminPage() {
-    return (
-        // Sidebar 
+  return (
+    // Sidebar
 
-        <div className='pt-[110px] w-[300px] h-screen bg-red-300 flex flex-col px-5'>
-            <h1>Hello</h1>
+    <div className="pt-[110px] w-[300px] h-screen bg-[#fcfaf6] flex flex-col px-5">
 
-            <p className='flex justify-start items-center gap-2 cursor-pointer text-[22px] '><FaUserGraduate />Candidates</p>
-            <p className='flex justify-start items-center gap-2 cursor-pointer text-[22px] '>< IoDocumentTextSharp />Applications</p>
-            <p className='flex justify-start items-center gap-2 cursor-pointer text-[22px] '><HiMiniBuildingOffice2 />Companies</p>
-            <p className='flex justify-start items-center gap-2 cursor-pointer text-[22px] '><MdWork />Jobs</p>
-            <p className='flex justify-start items-center gap-2 cursor-pointer text-[22px] '><FaPowerOff />Logout</p>
-        </div>
-    )
+        {/* Logo and Name  */}
+
+      <div className="flex flex-col justify-center items-center pb-20 gap-5">
+        <img src="/RheinlandLogoHeader.png" alt="" className="w-[150px]" />
+        <h1 className="font-bold text-[25px]">Dashboard</h1>
+      </div>
+
+      {/* Options  */}
+      <div className="flex flex-col gap-5 px-5">
+        
+        <p className="flex justify-start items-center gap-2 cursor-pointer font-regular hover:text-rheinland-red text-[22px] ">
+          <FaUserGraduate className="text-[28px] "/>
+          Candidates
+        </p>
+        <p className="flex justify-start items-center gap-2 cursor-pointer font-regular hover:text-rheinland-red text-[22px] ">
+          <IoDocumentTextSharp className="text-[28px] " />
+          Applications
+        </p>
+        <p className="flex justify-start items-center gap-2 cursor-pointer font-regular hover:text-rheinland-red text-[22px] ">
+          <HiMiniBuildingOffice2 className="text-[28px] " />
+          Companies
+        </p>
+        <p className="flex justify-start items-center gap-2 cursor-pointer font-regular hover:text-rheinland-red text-[22px] ">
+          <MdWork className="text-[28px] " />
+          Jobs
+        </p>
+        <p className="flex justify-start items-center gap-2 cursor-pointer font-regular hover:text-rheinland-red text-[22px] ">
+          <FaPowerOff className="text-[28px] " />
+          Logout
+        </p>
+      </div>
+    </div>
+  );
 }
 
-export default adminPage
+export default adminPage;
