c4a24f1e998f7cdb65ea66919d3cd1893a5202e3
Praveen Prasad <praveenprasad2510@gmail.com>
2024-06-26 15:20:06 +0530

    Added Sidebars with offices

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,17 +1,22 @@
 import React from 'react'
+import { FaUserGraduate } from "react-icons/fa";
+import { IoDocumentTextSharp } from "react-icons/io5";
+import { HiMiniBuildingOffice2 } from "react-icons/hi2";
+import { MdWork } from "react-icons/md";
+import { FaPowerOff } from "react-icons/fa";
 
 function adminPage() {
     return (
         // Sidebar 
 
         <div className='pt-[110px] w-[300px] h-screen bg-red-300 flex flex-col px-5'>
-            <h1>Hello</h1>  
+            <h1>Hello</h1>
 
-            <p>Candidates</p>
-            <p>Applications</p>
-            <p>Companies</p>
-            <p>Jobs</p>
-            <p>Logout</p>
+            <p className='flex justify-start items-center gap-2 cursor-pointer text-[22px] '><FaUserGraduate />Candidates</p>
+            <p className='flex justify-start items-center gap-2 cursor-pointer text-[22px] '>< IoDocumentTextSharp />Applications</p>
+            <p className='flex justify-start items-center gap-2 cursor-pointer text-[22px] '><HiMiniBuildingOffice2 />Companies</p>
+            <p className='flex justify-start items-center gap-2 cursor-pointer text-[22px] '><MdWork />Jobs</p>
+            <p className='flex justify-start items-center gap-2 cursor-pointer text-[22px] '><FaPowerOff />Logout</p>
         </div>
     )
 }
