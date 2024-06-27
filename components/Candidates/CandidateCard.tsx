3a478df277d8fa8fddbcbfb17ceac2f39abfb03c
Praveen Prasad <praveenprasad2510@gmail.com>
2024-06-28 00:20:53 +0530

    Completed Candidate Card

--- components/Candidates/CandidateCard.tsx
+++ components/Candidates/CandidateCard.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import Image from "next/image";
 import { IoLocationSharp } from "react-icons/io5";
+import { FaFileDownload } from "react-icons/fa";
 
 function CandidateCard() {
   return (
-    <div className="w-[300px] flex flex-col rounded-lg bg-red-400 justify-center items-center">
+    <div className="w-[300px] flex flex-col rounded-lg justify-center items-center py-5 px-2 border-2 shadow-xl shadow-rheinland-red">
       <div className="flex flex-col justify-center items-center ">
         <Image
           src="/person-icon.jpg"
@@ -13,25 +14,29 @@ function CandidateCard() {
           width={100}
           className="rounded-[50px]"
         />
-        <h1 className="text-[22px] font-semibold">Hadi Razal</h1>
+        <h1 className="text-[22px] font-semibold cursor-default">Hadi Razal</h1>
 
-        <p className="flex flex-row justify-center items-center text-[14px] text-gray-500">
+        <p className="flex flex-row justify-center items-center text-[14px] text-gray-500 cursor-default">
           <IoLocationSharp />
           Ponnani,Kerala
         </p>
-        <p className="flex flex-row justify-center items-center text-[14px] ">
+        <p className="flex flex-row justify-center items-center text-[14px] cursor-default">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi
           asperiores, nemo esse eum obcaecati tempora.
         </p>
       </div>
       <div className="flex flex-row gap-5 pt-5">
-        <p className="w-28 bg-red-600 flex justify-center items-center p-1 text-[18px] text-white">
+        <p className="w-28 bg-red-600 flex justify-center items-center p-1 text-[18px] text-white cursor-default">
           A1
         </p>
         {/* Change Content or Items Later  */}
-        <p className="w-28 bg-red-600 flex justify-center items-center p-1 text-[18px] text-white">
+        <p className="w-28 bg-red-600 flex justify-center items-center p-1 text-[18px] text-white cursor-default">
           Nurse
         </p>
+      </div>
+      <div className="pt-5 flex flex-col justify-center items-center gap-2">
+        <button className=" flex flex-row justify-center gap-2 items-center w-[200px] bg-rheinland-black text-white rounded-lg py-2">Download Resume<FaFileDownload  className="text-[20px]"/></button>
+        <p className="cursor-pointer">View Profile...</p>
       </div>
     </div>
   );
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,20 +14,24 @@ const config: Config = {
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
       screens: {
-        'xs': '376px',
-        'sm': '640px',
-        'md': '768px',
-        'lg': '1024px',
-        'xl': '1280px',
-        '2xl': '1536px'
+        xs: "376px",
+        sm: "640px",
+        md: "768px",
+        lg: "1024px",
+        xl: "1280px",
+        "2xl": "1536px",
       },
       colors: {
-        'rheinland-red': '#ED1C24',
-        'rheinland-yellow': '#FFDE00',
-        'rheinland-black': '#000000',
-        'rheinland-gray': '#333333',
-        'rheinland-blue': '#003399',
-      }
+        "rheinland-red": "#ED1C24",
+        "rheinland-yellow": "#FFDE00",
+        "rheinland-black": "#000000",
+        "rheinland-gray": "#333333",
+        "rheinland-blue": "#003399",
+      },
+      boxShadow: {
+        custom:
+          "0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06)",
+      },
     },
   },
   plugins: [],
