c5db7f68e3ecf7d5a80a779f6ae852cb87e2df92
Praveen Prasad <praveenprasad2510@gmail.com>
2024-06-27 23:49:25 +0530

    Added Header section for candidate Card

--- components/Candidates/CandidateCard.tsx
+++ components/Candidates/CandidateCard.tsx
@@ -1,16 +1,23 @@
 import React from "react";
 import Image from "next/image";
+import { IoLocationSharp } from "react-icons/io5";
 
 function CandidateCard() {
   return (
-    <div className="">
-      <div>
+    <div className="w-[300px]">
+      <div className="flex flex-col justify-center items-center ">
         <Image
-          src="/RheinlandLogoHeader.png"
+          src="/person-icon.jpg"
           alt="Rheinland Logo"
-          height={150}
-          width={150}
+          height={100}
+          width={100}
         />
+        <h1 className="text-[22px] font-semibold">Hadi Razal</h1>
+
+        <p className="flex flex-row justify-center items-center text-[14px] text-gray-500">
+          <IoLocationSharp />
+          Ponnani,Kerala
+        </p>
       </div>
     </div>
   );
