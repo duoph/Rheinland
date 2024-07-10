1cc2ce7e5ecbd1cedd043686eb493aa5ab4ec546
Praveen Prasad <praveenprasad2510@gmail.com>
2024-07-10 22:40:20 +0530

    Completed Styling of Application card

--- components/Candidates/Applications/ApplicationCard.tsx
+++ components/Candidates/Applications/ApplicationCard.tsx
@@ -6,32 +6,39 @@ import { MdNotInterested } from "react-icons/md";
 function ApplicationCard() {
   return (
     // Main container
-    <div className="flex flex-col">
+    <div className="flex flex-col w-[300px] h-[350px]   justify-around bg-[#f6eed8] rounded-sm pt-5">
       {/* Job and Company  */}
-      <div className="flex flex-col">
-        <h1>Full Stack Developer</h1>
-        <h3>Microsoft</h3>
+      <div className="flex flex-col gap-1">
+        <h1 className="text-[28px]">Full Stack Developer</h1>
+        <h3 className="text-[20px] text-rheinland-blue">Microsoft</h3>
       </div>
       {/* Personal Details  */}
-      <div className="flex flex-col">
+      <div className="flex flex-col gap-8">
         {/* Name and Highest German language level  */}
-        <div className="flex  flex-row">
-          <h1>Hadi Razal</h1>
+        <div className="flex  flex-row justify-between px-5">
+          <h1 className="text-[18px] text-rheinland-red">Hadi Razal</h1>
           <p>A2</p>
         </div>
         {/* Location & Experience */}
-        <div className="flex  flex-row">
+        <div className="flex  flex-row justify-between px-5">
           <h1 className="flex flex-row items-center">
-            <FaMapMarkerAlt />Ponnani
+            <FaMapMarkerAlt />
+            Ponnani
           </h1>
           <p>Fresher</p>
         </div>
       </div>
+      <div className="flex flex-col justify-center items-center ">
+        <p className="italic underline cursor-pointer">View Resume...</p>
+      </div>
       {/* Buttons  */}
-      <div className="flex flex-row gap-5">
-      <p>View Resume</p>
-      <button className="flex flex-row items-center">Accept <FaCheck /></button>
-      <button className="flex flex-row items-center">Reject <MdNotInterested /></button>
+      <div className="flex flex-row gap-5 justify-between bg-white p-5 m-2 rounded-sm">
+        <button className="flex flex-row items-center p-2 text-white rounded-sm bg-green-500 gap-3">
+          Accept <FaCheck />
+        </button>
+        <button className="flex flex-row items-center p-2 text-white rounded-sm bg-red-500 gap-3">
+          Reject <MdNotInterested />
+        </button>
       </div>
     </div>
   );
