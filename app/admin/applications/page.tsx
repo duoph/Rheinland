c10417394e928bb73e22736cc4824e7a69ec3aa7
Praveen Prasad <praveenprasad2510@gmail.com>
2024-07-10 22:56:14 +0530

    Updated Applications page with basic layout and pagination

--- app/admin/applications/page.tsx
+++ app/admin/applications/page.tsx
@@ -1,30 +1,56 @@
-import ApplicationCard from '@/components/Candidates/Applications/ApplicationCard'
-import React from 'react'
+import React, { useState } from "react";
+import ReactPaginate from "react-paginate";
+import { FaArrowCircleLeft } from "react-icons/fa";
+import { FaArrowCircleRight } from "react-icons/fa"; 
+import ApplicationCard from "@/components/Candidates/Applications/ApplicationCard";
 
-function page() {
+const candidates = Array(20).fill(null); // Assuming you have 20 candidates for this example
+
+function ApplicationPage() {
+  const [currentPage, setCurrentPage] = useState(0);
+  const itemsPerPage = 9;
+
+  const handlePageClick = (data: { selected: number }) => {
+    setCurrentPage(data.selected);
+  };
+
+  const offset = currentPage * itemsPerPage;
+  const currentCandidates = candidates.slice(offset, offset + itemsPerPage);
+  const pageCount = Math.ceil(candidates.length / itemsPerPage);
+
   return (
-    <div className='pt-[150px]  flex gap-2 flex-row flex-wrap'>
-      
-      <ApplicationCard/>
-      <ApplicationCard/>
-      <ApplicationCard/>
-      <ApplicationCard/>
-      <ApplicationCard/>
-      <ApplicationCard/>
-      <ApplicationCard/>
-      <ApplicationCard/>
-      <ApplicationCard/>
-      <ApplicationCard/>
-      <ApplicationCard/>
-      <ApplicationCard/>
-      <ApplicationCard/>
-      <ApplicationCard/>
-      <ApplicationCard/>
-      <ApplicationCard/>
-      <ApplicationCard/>
-      <ApplicationCard/>
+    <div className="py-[120px] flex flex-col items-center">
+      <div className="flex flex-row flex-wrap gap-5 justify-center items-center">
+        {currentCandidates.map((_, index) => (
+          <ApplicationCard key={index} />
+        ))}
+      </div>
+      <ReactPaginate
+        previousLabel={
+          <FaArrowCircleLeft className="text-[30px] text-rheinland-blue" />
+        }
+        nextLabel={
+          <FaArrowCircleRight className="text-[30px] text-rheinland-blue" />
+        }
+        breakLabel={"..."}
+        breakClassName={"break-me"}
+        pageCount={pageCount}
+        marginPagesDisplayed={2}
+        pageRangeDisplayed={5}
+        onPageChange={handlePageClick}
+        containerClassName={"pagination"}
+        pageClassName={"page-item"}
+        pageLinkClassName={"page-link"}
+        previousClassName={"page-item"}
+        previousLinkClassName={"page-link"}
+        nextClassName={"page-item"}
+        nextLinkClassName={"page-link"}
+        activeClassName={"activePage"}
+        disabledClassName={"disabledPage"}
+        className="flex flex-row gap-8 py-10 items-center text-[18px] "
+      />
     </div>
-  )
+  );
 }
 
-export default page
+export default ApplicationPage;
