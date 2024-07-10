11b2c12b928bd115500cfaf115ecafb87a1e2824
Praveen Prasad <praveenprasad2510@gmail.com>
2024-07-10 21:44:24 +0530

    Created New Components for Applications

---
+++ app/admin/applications/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function page() {
+  return (
+    <div className='pt-[150px]'>
+      <h1>Working</h1>
+    </div>
+  )
+}
+
+export default page
---
+++ components/Candidates/Applications/ApplicationCard.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ApplicationCard() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default ApplicationCard
