33d3cc492f2ac4728bb45a88c0c5913ae9ef489a
hadi-razal <hadhirasal22@gmail.com>
2024-07-18 22:49:14 +0530

    added alert slider marque

--- components/AlertMessageSlider.tsx
+++ components/AlertMessageSlider.tsx
@@ -3,10 +3,10 @@ import Marquee from 'react-fast-marquee';
 
 const AlertMessageSlider = () => {
     return (
-        <div className='fixed bg-rheinland-red top-[75px] left-0'>
-            <Marquee loop={0} pauseOnHover={true}>
+        <div className='fixed bg-rheinland-red top-[75px] left-0 w-full'>
+            <Marquee loop={0} speed={50} pauseOnHover={true}>
                 <h1 className='text-white font-extralight'>
-                    This website is still under development. The products listed are demo products . Follow us on Instagram  <a target='_blank' href="https://www.instagram.com/threadle_designs/" className="underline"> </a>
+                    This website is still under development. The Jobs listed are demo . Stay tuned..  <a target='_blank' href="https://www.instagram.com/threadle_designs/" className="underline"> </a>
                 </h1>
             </Marquee>
         </div>
