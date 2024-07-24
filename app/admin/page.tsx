26a27c99ac7a3856129f2a94ac0baf0d3ffc4ea7
Praveen Prasad <praveenprasad2510@gmail.com>
2024-07-24 23:58:12 +0530

    Updated code

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,10 +1,8 @@
-
 const AdminPage = () => {
-    return (
-        <div className="pt-[95px] flex items-center justify-center">
-            <h1>Admin Pannel</h1>
-        </div>
-
-    );
-}
+  return (
+    <div className="pt-[95px] flex items-center justify-center">
+      <h1>Admin Pannel</h1>
+    </div>
+  );
+};
 export default AdminPage;
