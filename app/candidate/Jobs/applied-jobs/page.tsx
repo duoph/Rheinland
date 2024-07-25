5a18304ccd64c157180c02e8b05e71687a45d7ac
Praveen Prasad <praveenprasad2510@gmail.com>
2024-07-25 10:19:30 +0530

    Removed Unnecessary Comments

--- app/candidate/Jobs/applied-jobs/page.tsx
+++ app/candidate/Jobs/applied-jobs/page.tsx
@@ -9,13 +9,8 @@ function AppliedJobs() {
   const [currentPage, setCurrentPage] = useState(0);
   const itemsPerPage = 9;
 
-  // const handlePageClick = (data: { selected: number }) => {
-  //   setCurrentPage(data.selected);
-  // };
-
   const offset = currentPage * itemsPerPage;
   const currentCandidates = candidates.slice(offset, offset + itemsPerPage);
-  // const pageCount = Math.ceil(candidates.length / itemsPerPage);
 
   return (
     <div className="pt-[95px] flex flex-col items-center">
