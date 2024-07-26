2901d08842ffbbfbc22478fdf997a6b22557ea3e
Praveen Prasad <praveenprasad2510@gmail.com>
2024-07-26 11:04:32 +0530

    completed Layout of Related Jobs card

---
+++ components/Related Jobs/Related Job Card/RelatedJobCard.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { FaMapMarkerAlt } from "react-icons/fa";
+import { FaCheck } from "react-icons/fa6";
+import { MdNotInterested } from "react-icons/md";
+import { FaRegUser } from "react-icons/fa";
+
+function RelatedJobCard() {
+  return (
+    <div>
+      <div className="flex flex-col bg-white p-5 rounded-sm w-[600px] h-[200px]">
+        <h1 className="text-[28px] font-sem">Full Stack Developer</h1>
+        <div className="flex flex-row justify-between">
+          <h3 className="text-[20px] text-rheinland-blue">Microsoft</h3>
+          <h1 className="flex flex-row items-center">
+            <FaMapMarkerAlt className="text-rheinland-red" />
+            Berlin
+          </h1>
+        </div>
+        {/* Number of Openings */}
+        <div className="flex flex-row">
+          <p className="flex flex-row items-center justify-center gap-2 text-gray-500">
+            <FaRegUser />
+            <span>10</span>openings
+          </p>
+        </div>
+        <div className="flex flex-col  pt-5 ">
+          <p className="italic underline cursor-pointer">More Details...</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default RelatedJobCard;
--- components/Related Jobs/RelatedJobs.tsx
+++ components/Related Jobs/RelatedJobs.tsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import RelatedJobCard from './Related Job Card/RelatedJobCard'
 
 function RelatedJobs() {
   return (
-    <div className='bg-[#f0f8ff] h-screen'>
-      <h1>Related Jobs</h1>
+    <div className='bg-[#f0f8ff] w-full p-20 flex flex-col justify-center items-center'>
+      <h1 className='text-2xl font-semibold'>Related Jobs</h1>
+      <div className='flex items-start w-full'>
+        <RelatedJobCard />
+      </div>
     </div>
   )
 }
