eb7f671c4cf7fbbaed5314bfabd6abb3bc0c71e0
Praveen Prasad <praveenprasad2510@gmail.com>
2024-08-04 10:37:10 +0530

    Started Admin panel

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -4,7 +4,9 @@ import AdminSliderMenu from "@/components/SliderMenus/adminSliderMenu";
 const AdminPage = () => {
   return (
     <div className="pt-[95px] flex flex-col items-center justify-center">
-      <h1>Admin Panel</h1>
+      <div>
+        
+      </div>
     </div>
   );
 };
