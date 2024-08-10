d746ee4bff7bc3ff24ccce919f7dd1a96955a993
hadi-razal <hadhirasal22@gmail.com>
2024-08-10 18:40:27 +0530

    related jobs compoenet updated

--- components/RelatedJobs/RelatedJobs.tsx
+++ components/RelatedJobs/RelatedJobs.tsx
@@ -1,35 +1,34 @@
 "use client";
 
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { Job } from "@/types";
 import JobCard from "../JobCard";
-import axios from "axios";
-
 
 interface RelatedJobsProps {
   jobs: Job[];
   loading: boolean;
 }
 
-
 function RelatedJobs({ jobs, loading }: RelatedJobsProps) {
 
   return (
-    <>
-      <h1 className="text-2xl font-semibold">Related Jobs</h1>
+    <div className="flex flex-col items-center justify-center gap-3 w-full">
+      <h1 className="text-3xl text-center font-semibold w-full">Related Jobs</h1>
 
       <div className="w-full gap-2 flex flex-col flex-wrap md:flex-row justify-center items-center">
-        {loading
-          ? Array.from({ length: 4 }).map((_, index) => (
+        {loading ? (
+          Array.from({ length: 4 }).map((_, index) => (
             <JobCard key={index} job={null} />
           ))
-          : jobs
-            .slice(0, 4)
-            .map((job, index) => (
-              <JobCard key={index} isLoading={loading} job={job} />
-            ))}
+        ) : jobs.length === 0 ? (
+          <p>No jobs available</p>
+        ) : (
+          jobs.slice(0, 4).map((job, index) => (
+            <JobCard key={index} isLoading={loading} job={job} />
+          ))
+        )}
       </div>
-    </>
+    </div>
   );
 }
 
