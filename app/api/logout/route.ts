38e528f66e166ee35d805154a8b1e281cc1e4b1f
duoph <153887780+duoph@users.noreply.github.com>
2024-10-02 12:11:19 +0530

    Update route.ts

--- app/api/logout/route.ts
+++ app/api/logout/route.ts
@@ -1,17 +1,22 @@
 import { NextResponse } from "next/server";
 
 export async function GET() {
-
     try {
-        const response = NextResponse.json({ message: "LogOut successful", success: true });
+        const response = NextResponse.json({
+            message: "LogOut successful",
+            success: true
+        });
 
+        // Delete cookies
         response.cookies.delete("token");
         response.cookies.delete("accountType");
 
         return response;
-
     } catch (error: any) {
-        console.log(error);
-        return NextResponse.json({ message: error.message, success: false });
+        console.error('Error during logout:', error);
+        return NextResponse.json({
+            message: error.message || "Internal Server Error",
+            success: false
+        });
     }
 }
