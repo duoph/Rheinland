d49d7ff23fedaf62cf500ea4459bc869b7fb8934
Hadi Razal <hadhirasal22@gmail.com>
2024-11-12 21:26:25 +0530

    seo updates page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,83 @@
 import CompanySignUpBanner from "@/components/Home/CompanySignUpBanner";
-import ExploteBycategory from "@/components/Home/ExploreBycategory";
 import NewAndFeaturedJobs from "@/components/Home/NewAndFeaturedJobs";
 import HelpedCompanies from "@/components/Home/HelpedCompanies";
 import HeroContainer from "@/components/Home/HeroContainer";
+import ExploreByCategory from "@/components/Home/ExploreByCategory";
+
+export const metadata = {
+  title: "Rheinland Jobs | Find Your Dream Job in Germany",
+  description: "Explore job opportunities in Germany with Rheinland Jobs. We connect skilled professionals with top German companies. Find your dream job today!",
+  keywords: [
+    "German jobs",
+    "jobs in Germany",
+    "Rheinland Jobs",
+    "job consultancy Germany",
+    "career opportunities Germany",
+    "recruitment Germany",
+    "top employers Germany",
+  ],
+  openGraph: {
+    title: "Rheinland Jobs | Find Your Dream Job in Germany",
+    description: "Explore job opportunities in Germany with Rheinland Jobs. Start your career journey today!",
+    url: "https://www.rheinlandconsultancy.com/",
+    images: [
+      {
+        url: "https://www.rheinlandconsultancy.com/RheinlandLogoHeader.png",
+        width: 800,
+        height: 600,
+        alt: "Rheinland Jobs Logo",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Rheinland Jobs | Find Your Dream Job in Germany",
+    description: "Find your ideal job in Germany with Rheinland Jobs. We connect skilled professionals with reputable German employers.",
+    images: ["https://www.rheinlandconsultancy.com/RheinlandLogoHeader.png"],
+  },
+};
 
 export default function Home() {
   return (
-    <div className="">
+    <main className="home-page">
+      {/* Structured Data for JSON-LD */}
+      <script
+        type="application/ld+json"
+        dangerouslySetInnerHTML={{
+          __html: JSON.stringify({
+            "@context": "https://schema.org",
+            "@type": "WebPage",
+            name: "Home",
+            description:
+              "Explore job opportunities in Germany with Rheinland Jobs. We connect skilled professionals with top German companies.",
+            url: "https://www.rheinlandconsultancy.com/",
+            potentialAction: {
+              "@type": "SearchAction",
+              target: "https://www.rheinlandconsultancy.com/search?q={search_term_string}",
+              "query-input": "required name=search_term_string",
+            },
+            mainEntity: {
+              "@type": "Organization",
+              name: "Rheinland Jobs",
+              logo: "https://www.rheinlandconsultancy.com/RheinlandLogoHeader.png",
+              url: "https://www.rheinlandconsultancy.com/",
+              sameAs: [
+                "https://www.facebook.com/rheinlandjobs",
+                "https://www.linkedin.com/company/rheinland-jobs",
+                "https://twitter.com/rheinland_jobs",
+              ],
+            },
+          }),
+        }}
+      />
       <HeroContainer />
       <HelpedCompanies />
-      <ExploteBycategory />
+      <ExploreByCategory />
       <CompanySignUpBanner />
       <NewAndFeaturedJobs />
-    </div>
+    </main>
   );
 }
+
